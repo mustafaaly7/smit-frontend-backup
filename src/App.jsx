be4c642a9eeb15ecece41aece6a9cb25624edcdf
MUHAMMAD ANAS <154446804+anashanif123@users.noreply.github.com>
2025-01-09 20:26:02 +0500

    add assignment route

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,15 @@ import Admin from "./admin/admin";
 import Teacher from "./teacher/teacher";
 import Student from "./student/student";
 import Login from "./login";
-
-import TeacherProfile from "./teacher/teacher-Profile";
-
 import AdminTeacher from "./admin/adminTeacher";
 import AdminTeacherDetails from "./admin/adminTeacherDetail";
 import AdminStudent from "./admin/adminStudent";
 
-
 import AdminCourse from "./admin/adminCourse";
 import ForgotPassword from "./forgotPassword";
-
-
+import Courses from "./student/studentCourse";
+import Assignments from "./student/studentAssignment";
+import StudentProfile from "./student/studentProfile";
 
 function App() {
   return (
@@ -22,24 +19,18 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route index element={<Login />} />
-
-
           <Route path="/teacher" element={<Teacher />} />
           <Route path="/student" element={<Student />} />
           <Route path="/admin" element={<Admin />} />
           <Route path="/adminTeacher" element={<AdminTeacher />} />
-          <Route path="/teacher/:id" element={<AdminTeacherDetails />} />  {/* The route for the Teacher Details page */}
+          <Route path="/teacher/:id" element={<AdminTeacherDetails />} />
           <Route path="/adminStudent" element={<AdminStudent />} />
-
-       
-
           <Route path="/adminCourse" element={<AdminCourse />} />
           <Route path="/forgotPassword" element={<ForgotPassword />} />
+          <Route path="/studentcourse" element={<Courses />} />
+          <Route path="/assignments/:courseId" element={<Assignments />} /> {/* Define the Assignments route */}
 
-
-
-
-          {/* Use :id here */}
+          <Route path="/studentProfile" element={<StudentProfile />} />
         </Routes>
       </BrowserRouter>
     </>
